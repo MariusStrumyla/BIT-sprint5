6580cab97ced2fc259693cb16ed6d3ee183b4b32
Marius Strumyla <marius.str@protonmail.com>
2022-05-19 21:33:15 +0300

    Added "edit item" function

--- src/Components/TodoApp/TodoApp.js
+++ src/Components/TodoApp/TodoApp.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function TodoApp() {
-  const [todos, setTodos] = React.useState([])
-  const [todo, setTodo] = React.useState('')
+  const [todos, setTodos] = React.useState([]);
+  const [todo, setTodo] = React.useState("");
+  const [todoEditing, setTodoEditing] = React.useState(null);
+  const [editingText, setEditingText] = React.useState("");
 
   function handleSubmit(e) {
-    e.preventDefault()
+    e.preventDefault();
 
     const newTodo = {
       id: new Date().getTime(),
@@ -13,14 +15,43 @@ function TodoApp() {
       completed: false,
     }
 
-    setTodos([...todos].concat(newTodo))
-    setTodo("")
+    setTodos([...todos].concat(newTodo));
+    setTodo("");
   }
+
 
   function deleteTodo(id) {
     const updatedTodos = [...todos].filter((todo) => todo.id !== id);
     setTodos(updatedTodos);
   }
+  
+
+  function toggleComplete(id) {
+    const updatedTodos = [...todos].map((todo) => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed;
+      }
+      return todo;
+    });
+    
+    setTodos(updatedTodos);
+  }
+
+
+  function editTodo(id) {
+    const updatedTodos = [...todos].map((todo) => {
+      if (todo.id === id) {
+        todo.text = editingText;
+      }
+      return todo;
+    });
+
+
+    setTodos(updatedTodos);
+    setTodoEditing(null);
+    setEditingText("");
+  }
+
 
   return (
     <div className="TodoApp">
@@ -29,11 +60,16 @@ function TodoApp() {
         <input type="text" onChange={(e) => setTodo(e.target.value)} value={todo} />
         <button type="submit">Add item</button>
       </form>
+
       {todos.map((todo) => <div key={todo.id}>
-          <div>{todo.text}</div>
+          {todoEditing === todo.id  ? (<input type="text" onChange = {(e) => setEditingText(e.target.value)} value = {editingText}/>) : (<div>{todo.text}</div>)}
 
           <button onClick={() => deleteTodo(todo.id)}>Delete item</button>
 
+          <input type="checkbox" onChange={() => toggleComplete(todo.id)} checked={todo.completed} />
+
+        {todoEditing === todo.id  ? (<button onClick = {() => editTodo(todo.id)}>Submit edit</button>) : (<button onClick = {() => setTodoEditing(todo.id)}>Edit</button>)}
+        
       </div>)}
     </div>
   );

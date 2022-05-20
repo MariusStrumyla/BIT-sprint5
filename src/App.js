10f4be0e47a46ddd4c5a68ac910333f20181352f
Marius Strumyla <marius.str@protonmail.com>
2022-05-20 14:13:05 +0300

    Added route option and links

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import TodoApp from './Components/TodoApp/TodoApp';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './App.css';
+import TodoApp from './Components/TodoApp/TodoApp';
+import Header from './Components/Header/Header';
+import About from './Components/About/About';
 
 function App() {
   return (
-    <div className="App">
-      <TodoApp />
-    </div>
+    <BrowserRouter>
+      <Header />
+    {/* <TodoApp /> */}
+      <Routes>
+        
+        <Route exact path="/TodoApp" element={<TodoApp />} />
+        <Route exact path="/About" element={<About />} />
+
+      </Routes>
+    </BrowserRouter>
   );
 }
 
